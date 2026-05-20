ab6afc04c502a630ff94e88670b28ef760db972d
SayyedSufiyan <iamsufiyansayyed@gmail.com>
2026-05-20 01:59:26 -0700

    Connect PostgreSQL database

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -96,11 +96,13 @@ CHANNEL_LAYERS = {
 
 
 # ================= DATABASE =================
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
